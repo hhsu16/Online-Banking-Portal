2129ad47343f86c46e9f313d72071b367d71953f
VamsiSrinivasCh <vamsisrinivas.chalamolu@sjsu.edu>
2021-03-30 14:22:08 -0700

    Defining User class and initializing database with Users class.

--- OnlineBankingSystem/Backend/src/main/java/web/api/models/User.java
+++ OnlineBankingSystem/Backend/src/main/java/web/api/models/User.java
@@ -1,4 +1,113 @@
 package web.api.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name="Users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long userId;
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String emailId;
+    private Date dateOfBirth;
+    private String address;
+    @Column(nullable = false, unique = true, length = 10)
+    private String contact;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String emailId, Date dateOfBirth, String address, String contact, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.contact = contact;
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailId='" + emailId + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", contact='" + contact + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
