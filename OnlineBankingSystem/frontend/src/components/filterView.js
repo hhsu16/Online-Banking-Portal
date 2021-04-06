8e52e60f498cb0a8c7703e306b34cbfa6ae7c54b
hhsu16 <huangkai.hsu@gmail.com>
2021-04-06 00:19:42 -0700

    adding internal and external page

---
+++ OnlineBankingSystem/frontend/src/components/filterView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class filterView extends Component {
+  render() {
+    return (
+      <div>
+        using api endpoints to render all transaction and different features.
+        Sorting by name, date, search bar
+      </div>
+    );
+  }
+}
+
+export default filterView;
